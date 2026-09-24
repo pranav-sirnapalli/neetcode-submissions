27376b17409108dd74e5d8e7661cdfea9ff916fb
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2026-09-24 16:38:34 -0400

    Add: number-of-provinces - submission-0

---
+++ Data Structures & Algorithms/number-of-provinces/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        res = 0
+
+        def dfs(node):
+            isConnected[node][node] = 0
+
+            for nei in range(n):
+                if nei != node and isConnected[node][nei] and isConnected[nei][nei]:
+                    dfs(nei)
+
+
+        for i in range(n):
+            if isConnected[i][i]:
+                dfs(i)
+                res += 1
+        return res
